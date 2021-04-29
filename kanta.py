a9ec631220ede78d9270607ebbc427d7d547cf97
Angel <araozangel842@gmail.com>
2021-04-29 10:04:24 +0300

    Addings new view

--- kanta.py
+++ kanta.py
@@ -9,8 +9,6 @@ tietokannan_nimi = 'painonhallinta.db'
 
 def luo_tietokanta(tiedosto):
     """Luo tietokannan huom. tiedoston tyyppi po. .db
-    
-
     Args:
         tiedosto (string): SQL Lite tietokantatiedoston nimi
     """
@@ -31,7 +29,7 @@ def luo_taulut(tiedosto):
         sukupuoli INTEGER NOT NULL,
         spaiva DATE NOT NULL);''')
 
-    # Luodaan Mittaukset-taulu
+    # Luodaan Mittaukset-taulu, mittaus_id on laskuri
     yhteys.execute('''CREATE TABLE mittaus 
         (mittaus_id INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL,
         henkilo_id INTEGER NOT NULL,
@@ -48,7 +46,6 @@ def luo_taulut(tiedosto):
 # Luodaan testidataa
 def lisaa_henkilo(tiedosto, etunimi, sukunimi, sukupuoli, spaiva):
     """Lisätään argumenttina annetun tietokannan henkilo-tauluun uusi tietue
-
     Args:
         tiedosto (string): tietokantatiedoston nimi
         etunimi (string): Henkilön etunimi
@@ -71,26 +68,22 @@ def lisaa_henkilo(tiedosto, etunimi, sukunimi, sukupuoli, spaiva):
     # Suljetaan yheys
     yhteys.close()
 
-# TODO : luop funktio, jolla saadan puolilainausmerkit mekkkijononen ympärille
-
+# Funktio, jolla saadaan puolilainausmerkit merkkijonon ympärille
 def sql_string(kentta):
-    kentaa = "'" + kentta + "'"
+    kentta = "'" + kentta +"'"
     return kentta
 
-
-# TODO: Rutiini mittaustitojen syöttämiseksi mittaukset tauluun
-def lissa_mittaus(tiedosto, henkilo_id, pituus, paino):
-    """Lisää henkilön mittaustiedot mittaus-tauluun
-
+# Rutiini mittaustietojen syöttämiseksi mittaukset tauluun, huom. mittaus_id laskuri
+def lisaa_mittaus(tiedosto, henkilo_id, pituus, paino):
+    """Lisää henkilön mittaustiedot mittaus-tauluuun
     Args:
-        henkilo_id (integer): henkilö id
+        henkilo_id (integer): henkiön id
         pituus (float): henkilön pituus sentteinä
         paino (float): henkilön paino kiloina
     """
+    sql_lause = "INSERT INTO mittaus (henkilo_id, pituus, paino) VALUES (" + str(henkilo_id) + "," + str(pituus) + "," + str(paino) + ");"
 
-    sql_lause = "INSERT INTO mittaus (henkilo_id, pituus, paino) VALUES (" + str(henkilo_id) + "," + str(pituus) + "," + str(paino) + ")"
-
-    # Luodaan yhteys tietokantaan
+     # Luodaan yhteys tietokantaan
     yhteys = sqlite3.connect(tiedosto)
 
     # Suoritetaan tietueen lisäys SQL-lauseena
@@ -105,61 +98,55 @@ def lissa_mittaus(tiedosto, henkilo_id, pituus, paino):
 # TODO: luo rutiini tietojen lukemiseksi molemmista tauluita
 def lue_kaikki(tiedosto, taulu):
     """[summary]
-
     Args:
         tiedosto (string): tietokantatiedoston nimi
         taulu (string): taulun nimi
-
     Returns:
-        list: tulosjoukon
+        list: tulosjoukon tietueet
     """
-    lista =[]
+    lista = []
     sql_lause = "SELECT * FROM " + taulu + ";"
 
-    # Luodaan yhteys tietokantaan
+     # Luodaan yhteys tietokantaan
     yhteys = sqlite3.connect(tiedosto)
 
     # Suoritetaan tietueen lisäys SQL-lauseena
     tulosjoukko = yhteys.execute(sql_lause)
     for rivi in tulosjoukko:
         lista.append(rivi)
-
-    # Vahvistetaan tapahtuma (transaktio)
-    yhteys.commit()
     
     # Suljetaan yheys
     yhteys.close()
-    return lista
-    
+
+    return lista   
 
 # Paikallinen testaus
 if __name__ == "__main__":
     # luo_tietokanta(tietokannan_nimi)
     # luo_taulut(tietokannan_nimi)
 
-    """ etunimi = 'Mikko'
+    '''
+    etunimi = 'Mikko'
     sukunimi = 'Viljanen'
     sukupuoli = 1
     spaiva = '1968-12-03'
     sql_lause = "INSERT INTO henkilo (etunimi, sukunimi, sukupuoli, spaiva) VALUES (" + "'" + etunimi + "', " + "'" + sukunimi + "', " + str(sukupuoli) + ", " + "'" + spaiva + "');"
-    print(sql_lause) """
+    print(sql_lause) '''
 
     # lisaa_henkilo(tietokannan_nimi, 'Mikko', 'Viljanen', 1, '1968-12-03')
     # lisaa_henkilo(tietokannan_nimi, 'Mika', 'Vainio', 1, '1962-06-26')
 
-
-    """  
+    '''
     henkilo_id = 1
-    pituus= 171
-    paino= 74
-    sql_lause = "INSERT INTO mittaus (henkilo_id, pituus, paino) VALUES (" + str(henkilo_id) + "," + str(pituus) + "," + str(paino) + ")"
-    print(sql_lause) 
-    """
+    pituus = 171
+    paino = 74
+    sql_lause = "INSERT INTO mittaus (henkilo_id, pituus, paino) VALUES (" + str(henkilo_id) + "," + str(pituus) + "," + str(paino) + ");"
+    print(sql_lause) '''
 
-    # lissa_mittaus(tietokannan_nimi, 2, 171, 74)
+    # lisaa_mittaus(tietokannan_nimi, 2, 171, 74)
 
     tulosjoukko = lue_kaikki(tietokannan_nimi, 'henkilo')
     print(tulosjoukko)
-    #BUG: ei löydä näkymää, selvitä, mikä mättää 
-    tulosjoukko2 = lue_kaikki(tietokannan_nimi, 'henkilot_ja_mittauskset')
+    # Näkymän testaus: näkymä tehty GUI:ssa, muista tallentaa ja sulkea ennen testaamista!
+    tulosjoukko2 = lue_kaikki(tietokannan_nimi, 'henkilon_mittaukset')
     print(tulosjoukko2)
